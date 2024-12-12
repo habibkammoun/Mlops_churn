4c7472acf3dd48f3b128734bcbab3f1c572dd5d4
rami4real <belguith.ahmedrami@gmail.com>
2024-12-12 12:01:57 +0100

    Renderbv4

--- backend/main.py
+++ backend/main.py
@@ -88,7 +88,6 @@ def predict(data: ChurnPredictionModel):
     # Return predictions as JSON
     return {"predictions": predictions.tolist()}
 
-
-
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+
